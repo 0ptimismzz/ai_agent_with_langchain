b269cd09f3a12ecc026b137dbc45ec01d8ea3ba1
0ptimismzz <805583085@qq.com>
2025-08-04 18:49:37 +0800

    add agent SystemMessage_Prompt

--- app/code_agent/agent/code_agent.py
+++ app/code_agent/agent/code_agent.py
@@ -1,7 +1,8 @@
 import asyncio
 import time
 
-from langchain_core.messages import AIMessage, ToolMessage
+from langchain_core.messages import AIMessage, ToolMessage, SystemMessage
+from langchain_core.prompts import PromptTemplate
 from langchain_core.runnables import RunnableConfig
 from langgraph.prebuilt import create_react_agent
 
@@ -33,14 +34,27 @@ async def run_agent():
     terminal_tools = await get_stdio_terminal_tools()
     tools = file_tools + terminal_tools
 
+    prompt = PromptTemplate.from_template(template="""
+# 角色
+你是一名优秀的工程师，你的名字叫做{name}
+
+# 规范
+## 使用终端工具执行shell命令的步骤
+- 步骤1:调用*关闭终端*工具 close_terminal 关闭所有终端
+- 步骤2:打开一个新的终端，调用*打开终端*工具 open_terminal 打开一个新的终端
+- 步骤3:向终端输入命令，调用*运行终端脚本工具* run_script_in_terminal
+- 步骤4:查看终端命令执行结构，调用*获取终端文本工具* get_terminal_full_text
+""")
+
     agent = create_react_agent(
         model=llm_qwen,
         tools=tools,
         checkpointer=memory,
         debug=False,
+        prompt=SystemMessage(content=prompt.format(name="Bot")),
     )
 
-    config = RunnableConfig(configurable={"thread_id":3})
+    config = RunnableConfig(configurable={"thread_id":10})
 
     while True:
         user_input = input("用户：")
@@ -53,6 +67,9 @@ async def run_agent():
         iteration_count = 0
         start_time = time.time()
         last_tool_time = start_time
+
+        # 从RAG知识库中读取知识，并拼接到知识库中
+
 
         # res = await agent.ainvoke(input={"messages": user_input}, config=config)
         async for chunk in agent.astream(input={"messages": user_input}, config=config):
